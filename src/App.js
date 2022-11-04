0460231b50ba65037245b7fa7cb0e4a582ba0002
rbn_setiyawan <rbn.setiyawan@gmail.com>
2022-11-04 16:46:20 +0700

    Finishing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Work from './components/work/Work';
 import Testimonials from './components/testimonilas/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
+
 const App = () => {
   return (
     <>
@@ -20,6 +22,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Testimonials />
         <Contact />
       </main>
